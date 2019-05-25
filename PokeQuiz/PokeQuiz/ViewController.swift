dd8dab4b953107a4187ad8d392a3a0f3f193289e
khayashida <>
2019-05-25 16:00:34 +0900

    add Project

---
+++ PokeQuiz/PokeQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeQuiz
+//
+//  Created by khayashida on 2019/05/25.
+//  Copyright © 2019 jp.co.khayashida.ARCompanyCard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
